07055b62f98bc5c62f4a856544eab8723cfd3349
vicky <s913018@gmail.com>
2022-07-25 13:39:25 +0800

    title

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    public function hello(){
+return 'hello';
+    }
+    public function hello2(){
+      DoReMiFaSol
+    }
+}
